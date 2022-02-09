251f2e34a4a2cc49f9706cafd33f83916dda6977
Logan Begley <91550229+loganb2008@users.noreply.github.com>
2022-02-09 10:59:40 -0500

    Add ability to specify useragent

--- UserAgentDetect.js
+++ UserAgentDetect.js
@@ -1,53 +1,52 @@
-    function detectUa() { 
-    if(navigator.userAgent.indexOf("Nintendo Wii") != -1 ) // Wii
+    function detectUa(ua) { 
+if(ua == "" || ua == undefined || ua == null) {
+ua = navigator.userAgent;
+}
+    if(ua.indexOf("Nintendo Wii") != -1 ) // Wii
     {
         return "Wii"
     }
-    else if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) // Opera
+    else if((ua.indexOf("Opera") || ua.indexOf('OPR')) != -1 ) // Opera
     {
         return "Opera";
     }
-    else if(navigator.userAgent.indexOf("Edg") != -1 ) // Edge
+    else if(ua.indexOf("Edg") != -1 ) // Edge
     {
         return "Edge";
     }
-    else if(navigator.userAgent.indexOf("UCBrowser") != -1 ) // Chrome
-    {
-        return "UC";
-    }
-    else if(navigator.userAgent.indexOf("MQQBrowser") != -1 ) // Chrome
+    else if(ua.indexOf("MQQBrowser") != -1 ) // Chrome
     {
         return "QQ";
     }
-    else if(navigator.userAgent.indexOf("Maxthon") != -1 ) // Chrome
+    else if(ua.indexOf("Maxthon") != -1 ) // Chrome
     {
         return "Maxthon";
     }
-    else if(navigator.userAgent.indexOf("YaBrowser") != -1 ) // Chrome
+    else if(ua.indexOf("YaBrowser") != -1 ) // Chrome
     {
         return "Yandex";
     }
-    else if(navigator.userAgent.indexOf("Lynx") != -1 ) // Chrome
+    else if(ua.indexOf("Lynx") != -1 ) // Chrome
     {
         return "Lynx";
     }
-    else if(navigator.userAgent.indexOf("Chrome") != -1 ) // Chrome
+    else if(ua.indexOf("Chrome") != -1 ) // Chrome
     {
         return "Chrome";
     }
-    else if(navigator.userAgent.indexOf("Safari") != -1) // Safari
+    else if(ua.indexOf("Safari") != -1) // Safari
     {
         return "Safari";
     }
-    else if(navigator.userAgent.indexOf("Firefox") != -1 ) // Firefox
+    else if(ua.indexOf("Firefox") != -1 ) // Firefox
     {
         return "Firefox";
     }
-    else if(navigator.userAgent.indexOf("PLAYSTATION") != -1 ) // Playstation
+    else if(ua.indexOf("PLAYSTATION") != -1 ) // Playstation
     {
         return "Playstation"
     }
-    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) // IE
+    else if((ua.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) // IE
     {
       return "Internet Explorer";
     }  
