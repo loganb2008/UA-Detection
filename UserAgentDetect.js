f4276f83f2b9c5df021e18c8246a42de6bcb2593
Logan Begley <91550229+loganb2008@users.noreply.github.com>
2022-02-09 08:08:37 -0500

    Create UserAgentDetect.js

---
+++ UserAgentDetect.js
@@ -0,0 +1,38 @@
+    function detectUa() { 
+    if(navigator.userAgent.indexOf("Nintendo Wii") != -1 ) // Wii
+    {
+        return "Wii"
+    }
+    else if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) // Opera
+    {
+        return "Opera";
+    }
+    else if(navigator.userAgent.indexOf("Edg") != -1 ) // Edge
+    {
+        return "Edge";
+    }
+    else if(navigator.userAgent.indexOf("Chrome") != -1 ) // Chrome
+    {
+        return "Chrome";
+    }
+    else if(navigator.userAgent.indexOf("Safari") != -1) // Safari
+    {
+        return "Safari";
+    }
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) // Firefox
+    {
+        return "Firefox";
+    }
+    else if(navigator.userAgent.indexOf("PLAYSTATION") != -1 ) // Playstation
+    {
+        return "Playstation"
+    }
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) // IE
+    {
+      return "Internet Explorer";
+    }  
+    else // Others
+    {
+       return "Other";
+    }
+    }
